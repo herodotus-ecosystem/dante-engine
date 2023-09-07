f5fcf9ee8a8c3900e7000ea498b4e2c3e6105f37
jokeway <jokeway@hotmail.com>
2023-09-07 09:21:41 +0000

    fix：websocket消息没有正确发送出去。  主要参考：AbstractBrokerMessageHandler 的 checkDestinationPrefix 校验目标destination需要以配置的destinationPrefixes开头，否则会忽略消息发送。
    Signed-off-by: jokeway <jokeway@hotmail.com>

--- engine-message/message-sdk-websocket/src/main/java/cn/herodotus/engine/message/websocket/configuration/WebSocketMessageBrokerConfiguration.java
+++ engine-message/message-sdk-websocket/src/main/java/cn/herodotus/engine/message/websocket/configuration/WebSocketMessageBrokerConfiguration.java
@@ -140,7 +140,7 @@ public class WebSocketMessageBrokerConfiguration extends AbstractSessionWebSocke
          * 3. 可以配置心跳线程调度器 setHeartbeatValue这个不能单独设置，不然不起作用，要配合setTaskScheduler才可以生效
          *    调度器我们可以自己写一个，也可以自己使用默认的调度器 new DefaultManagedTaskScheduler()
          */
-        registry.enableSimpleBroker(MessageConstants.WEBSOCKET_CHANNEL_PROXY_BROADCAST, MessageConstants.WEBSOCKET_CHANNEL_PROXY_PERSONAL)
+        registry.enableSimpleBroker(ArrayUtils.addAll(webSocketProperties.getApplicationPrefixes(), webSocketProperties.getUserDestinationPrefix()))
                 .setHeartbeatValue(new long[]{10000, 10000})
                 .setTaskScheduler(taskScheduler);
 
