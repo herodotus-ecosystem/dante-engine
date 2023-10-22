37d772677d31b34b629bd4df04feda2daeb43a6b
码匠君 <pointer_v@qq.com>
2023-10-22 21:15:05 +0800

    📄 Update Copyright

--- engine-rest/rest-sdk-condition/src/main/java/cn/herodotus/engine/rest/condition/definition/LocalDataAccessCondition.java
+++ engine-rest/rest-sdk-condition/src/main/java/cn/herodotus/engine/rest/condition/definition/LocalDataAccessCondition.java
@@ -1,11 +1,17 @@
 /*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * Copyright (c) 2020-2030 郑庚伟 ZHENGGENGWEI (码匠君) (herodotus@aliyun.com & www.herodotus.cn)
  *
- * Copyright 2019-2022 ZHENGGENGWEI<码匠君>. All rights reserved.
+ * Dante Engine licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * - Author: ZHENGGENGWEI<码匠君>
- * - Contact: herodotus@aliyun.com
- * - Blog and source code availability: https://gitee.com/herodotus/herodotus-cloud
+ * <http://www.gnu.org/licenses/lgpl.html>
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package cn.herodotus.engine.rest.condition.definition;
