68fa43d2be240f9ee9baff42f72ab30493d3bb31
码匠君 <pointer_v@qq.com>
2023-11-27 12:25:22 +0800

    🌱 test: v3.2.0-RC2
    - 主要更新
      - [验证] 完成 Spring Cloud Alibaba 在 Spring Boot 3.2 和 Spring Cloud 2023 最新环境的验证。
    - 其它更新
      - [重构] 重构 RestTemplate 和 OpenFeign 底层 Engine 及负载均衡统一化配置。去除 Spring Boot 3.2 不再支持的 OkHttp3ClientHttpRequestFactory 相关配置，增加基于 JdkClient 的 RestTemplate 和 OpenFeign 统一配置。fix: #I8JNOK
      - [适配] 适配 Spring Cloud Alibaba 生态组件。临时去除 Sentinel 相关组件依赖和代码，解决在 Spring Cloud 2023.0.0 环境下，依赖 Sentinel 会引起 Feign 契约配置失效而导致的服务无法启动问题。

--- engine-facility/facility-alibaba-spring-boot-starter/src/main/java/cn/herodotus/engine/facility/alibaba/autoconfigure/sentinel/FacilityAlibabaSentinelAutoConfiguration.java
+++ engine-facility/facility-alibaba-spring-boot-starter/src/main/java/cn/herodotus/engine/facility/alibaba/autoconfigure/sentinel/FacilityAlibabaSentinelAutoConfiguration.java
@@ -16,29 +16,11 @@
 
 package cn.herodotus.engine.facility.alibaba.autoconfigure.sentinel;
 
-import cn.herodotus.engine.assistant.core.domain.Result;
-import cn.herodotus.engine.assistant.core.json.jackson2.utils.Jackson2Utils;
-import cn.herodotus.engine.facility.alibaba.autoconfigure.sentinel.enhance.HerodotusSentinelFeign;
-import com.alibaba.cloud.sentinel.feign.SentinelFeignAutoConfiguration;
-import com.alibaba.csp.sentinel.SphU;
-import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.BlockRequestHandler;
-import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
-import feign.Feign;
 import jakarta.annotation.PostConstruct;
-import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 /**
  * <p>Description: 基础设施 Sentinel 配置 </p>
@@ -46,8 +28,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
  * @author : gengwei.zheng
  * @date : 2022/2/5 17:57
  */
-@AutoConfiguration(before = SentinelFeignAutoConfiguration.class)
-@ConditionalOnClass({SphU.class, Feign.class})
+@AutoConfiguration
+@ConditionalOnClass
 public class FacilityAlibabaSentinelAutoConfiguration {
 
     private static final Logger log = LoggerFactory.getLogger(FacilityAlibabaSentinelAutoConfiguration.class);
@@ -57,43 +39,43 @@ public class FacilityAlibabaSentinelAutoConfiguration {
         log.info("[Herodotus] |- Module [Facility Alibaba Sentinel Starter] Auto Configure.");
     }
 
-    @Bean
-    @Scope("prototype")
-    @ConditionalOnMissingBean
-    @ConditionalOnProperty(name = "feign.sentinel.enabled")
-    public Feign.Builder feignSentinelBuilder() {
-        return HerodotusSentinelFeign.builder();
-    }
-
-    /**
-     * 限流、熔断统一处理类
-     */
-    @Configuration(proxyBeanMethods = false)
-    @ConditionalOnClass(HttpServletRequest.class)
-    public static class WebmvcHandler {
-        @Bean
-        public BlockExceptionHandler webmvcBlockExceptionHandler() {
-            return (request, response, e) -> {
-                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-                Result<String> result = Result.failure("Too many request, please retry later.");
-                response.getWriter().print(Jackson2Utils.toJson(result));
-            };
-        }
-
-    }
-
-    /**
-     * 限流、熔断统一处理类
-     */
-    @Configuration(proxyBeanMethods = false)
-    @ConditionalOnClass(ServerResponse.class)
-    public static class WebfluxHandler {
-        @Bean
-        public BlockRequestHandler webfluxBlockExceptionHandler() {
-            return (exchange, t) ->
-                    ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .body(BodyInserters.fromValue(Result.failure(t.getMessage())));
-        }
-    }
+//    @Bean
+//    @Scope("prototype")
+//    @ConditionalOnMissingBean
+//    @ConditionalOnProperty(name = "feign.sentinel.enabled")
+//    public Feign.Builder feignSentinelBuilder() {
+//        return HerodotusSentinelFeign.builder();
+//    }
+//
+//    /**
+//     * 限流、熔断统一处理类
+//     */
+//    @Configuration(proxyBeanMethods = false)
+//    @ConditionalOnClass(HttpServletRequest.class)
+//    public static class WebmvcHandler {
+//        @Bean
+//        public BlockExceptionHandler webmvcBlockExceptionHandler() {
+//            return (request, response, e) -> {
+//                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+//                Result<String> result = Result.failure("Too many request, please retry later.");
+//                response.getWriter().print(Jackson2Utils.toJson(result));
+//            };
+//        }
+//
+//    }
+//
+//    /**
+//     * 限流、熔断统一处理类
+//     */
+//    @Configuration(proxyBeanMethods = false)
+//    @ConditionalOnClass(ServerResponse.class)
+//    public static class WebfluxHandler {
+//        @Bean
+//        public BlockRequestHandler webfluxBlockExceptionHandler() {
+//            return (exchange, t) ->
+//                    ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
+//                            .contentType(MediaType.APPLICATION_JSON)
+//                            .body(BodyInserters.fromValue(Result.failure(t.getMessage())));
+//        }
+//    }
 }
