1ed37040ae73a7eb8c76482b3dce745bcec77073
码匠君 <pointer_v@qq.com>
2023-12-03 12:59:42 +0800

    [升级] springdoc 版本升级至 2.3.0

--- engine-message/message-core/src/main/java/cn/herodotus/engine/message/core/constants/HerodotusChannels.java
+++ engine-message/message-core/src/main/java/cn/herodotus/engine/message/core/constants/HerodotusChannels.java
@@ -24,8 +24,24 @@ package cn.herodotus.engine.message.core.constants;
  */
 public interface HerodotusChannels {
 
+    /**
+     * Mqtt 默认入站通道
+     */
     String MQTT_DEFAULT_INBOUND_CHANNEL = "mqttDefaultInboundChannel";
+    /**
+     * Mqtt 默认出站通道
+     */
     String MQTT_DEFAULT_OUTBOUND_CHANNEL = "mqttDefaultOutboundChannel";
-
-    String EMQX_MONITOR_INBOUND_CHANNEL = "emqxMonitorInboundChannel";
+    /**
+     * Emqx 默认的监控指标数据数据 Mqtt 类型入站通道
+     */
+    String EMQX_DEFAULT_MONITOR_MQTT_INBOUND_CHANNEL = "emqxDefaultMonitorMqttInboundChannel";
+    /**
+     * Emqx 默认的 Webhook 数据 HTTP 类型入站通道
+     */
+    String EMQX_DEFAULT_WEBHOOK_HTTP_INBOUND_CHANNEL = "emqxDefaultWebhookHttpInboundChannel";
+    /**
+     * Emqx 默认的 Webhook 数据 EVENT 类型出站通道
+     */
+    String EMQX_DEFAULT_WEBHOOK_EVENT_OUTBOUND_CHANNEL = "emqxDefaultWebhookHttpOutboundChannel";
 }
