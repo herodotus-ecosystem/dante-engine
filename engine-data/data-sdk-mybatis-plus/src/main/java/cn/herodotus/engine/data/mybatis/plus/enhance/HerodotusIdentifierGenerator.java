fbaf135537869f6ecbca838a438c6c2a25d5ebfa
码匠君 <pointer_v@qq.com>
2023-12-24 21:27:48 +0800

    🚑 fix: v3.2.1.1
    - 主要更新
      - [升级] spring-boot-admin 版本升级至 3.2.0
      - [修复] 采用临时版本，解决 mybatis-plus 与 Spring Boot 3.1.7 和 3.2.1 版本不兼容，启动抛出 Bean named 'ddlApplicationRunner' is expected to be of type 'org.springframework.boot.Runner' but was actually of type 'org.springframework.beans.factory.support.NullBean' 错误问题
    - 其它更新
      - [升级] Camunda 版本升级至 2.5
      - [升级] minio docker 镜像版本升级至 RELEASE.2023-12-23T07-19-11Z
      - [新增] 在线文档和项目 Readme 新增出厂安全测试说明
      - [优化] 代码适配 Hutool 6.0.0-M9
    - 依赖更新
      - [升级] aws-java-sdk-s3 版本升级至 1.12.625
      - [升级] hutool 版本升级至 6.0.0-M9
      - [升级] fastjson2 版本升级至 2.0.44

--- engine-data/data-sdk-mybatis-plus/src/main/java/cn/herodotus/engine/data/mybatis/plus/enhance/HerodotusIdentifierGenerator.java
+++ engine-data/data-sdk-mybatis-plus/src/main/java/cn/herodotus/engine/data/mybatis/plus/enhance/HerodotusIdentifierGenerator.java
@@ -34,6 +34,6 @@ public class HerodotusIdentifierGenerator implements IdentifierGenerator {
     public Number nextId(Object entity) {
         // 采用雪花算法获取id,时间回拨会存在重复,这里用随机数来减少重复的概率
         final Snowflake snowflake = IdUtil.getSnowflake(1, (int) (Math.random() * 20 + 1));
-        return snowflake.nextId();
+        return snowflake.next();
     }
 }
