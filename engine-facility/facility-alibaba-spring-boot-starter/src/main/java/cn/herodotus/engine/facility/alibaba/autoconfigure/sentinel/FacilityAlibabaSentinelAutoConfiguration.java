8f56074e41e927a528d009a67b38aa788965980d
码匠君 <pointer_v@qq.com>
2024-02-20 22:03:50 +0800

    🍻 fix: v3.1.8.4
    - 主要更新
      - [升级] Spring Cloud Tencent 版本升级至 1.13.1-2022.0.4
      - [升级] Spring Boot Admin 版本升级至 3.2.2
      - [回滚] Spring Cloud 版本降级至 2022.0.4
    - 其它更新
      - [重构] 代码适配 Hutool 6.0.0-M11
      - [回滚] 回滚 Spring Cloud 版本至 2022.0.4，以解决新版本 Spring Cloud 升级 Feign 版本导致的 Spring Cloud Tencent 版本不兼容问题
      - [修复] 修复前端 tsParticles 代码问题，导致整个前端无法运行问题
      - [修复] 修复前端静态路由自动校验错误
      - [修复] 修复伴随 Spring Boot 版本，起的 Netty 版本升级，导致的 Spring Cloud Tencent 代码不兼容运行出错问题。
      - [修复] 修复 Spring Cloud Tencent 配置逻辑问题，导致服务启动出现 `The bean 'restTemplateCustomizer', defined in class path resource [com/tencent/cloud/polaris/loadbalancer/PolarisLoadBalancerAutoConfiguration.class], could not be registered. A bean with that name has already been defined in class path resource [org/springframework/cloud/client/loadbalancer/LoadBalancerAutoConfiguration$RetryInterceptorAutoConfiguration.class] and overriding is disabled.`
      - [优化] 优化以 Spring Cloud Tencent 作为微服务基础设置的模式下日志输出内容
      - [升级] minio docker 镜像版本升级至 RELEASE.2024-02-17T01-15-57Z
    - 依赖升级
      - [升级] redisson 版本升级至 3.27.0
      - [升级] minio 版本升级至 8.5.8
      - [升级] aws-java-sdk-s3 版本升级至 1.12.661
      - [升级] hutool 版本升级至 6.0.0-M11
      - [升级] com.baidu.aip 版本升级至 4.16.18
      - [升级] org.json 版本升级至 20240205
      - [升级] okio 版本升级至 3.8.0

--- engine-facility/facility-alibaba-spring-boot-starter/src/main/java/cn/herodotus/engine/facility/alibaba/autoconfigure/sentinel/FacilityAlibabaSentinelAutoConfiguration.java
+++ engine-facility/facility-alibaba-spring-boot-starter/src/main/java/cn/herodotus/engine/facility/alibaba/autoconfigure/sentinel/FacilityAlibabaSentinelAutoConfiguration.java
@@ -16,10 +16,27 @@
 
 package cn.herodotus.engine.facility.alibaba.autoconfigure.sentinel;
 
+import cn.herodotus.engine.assistant.core.domain.Result;
+import cn.herodotus.engine.assistant.core.json.jackson2.utils.Jackson2Utils;
+import cn.herodotus.engine.facility.alibaba.autoconfigure.sentinel.enhance.HerodotusSentinelFeign;
+import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import feign.Feign;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 /**
  * <p>Description: 基础设施 Sentinel 配置 </p>
@@ -37,43 +54,43 @@ public class FacilityAlibabaSentinelAutoConfiguration {
         log.info("[Herodotus] |- Module [Facility Alibaba Sentinel Starter] Auto Configure.");
     }
 
-//    @Bean
-//    @Scope("prototype")
-//    @ConditionalOnMissingBean
-//    @ConditionalOnProperty(name = "feign.sentinel.enabled")
-//    public Feign.Builder feignSentinelBuilder() {
-//        return HerodotusSentinelFeign.builder();
-//    }
+    @Bean
+    @Scope("prototype")
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(name = "feign.sentinel.enabled")
+    public Feign.Builder feignSentinelBuilder() {
+        return HerodotusSentinelFeign.builder();
+    }
 
     /**
      * 限流、熔断统一处理类
      */
-//    @Configuration(proxyBeanMethods = false)
-//    @ConditionalOnClass(HttpServletRequest.class)
-//    public static class WebmvcHandler {
-//        @Bean
-//        public BlockExceptionHandler webmvcBlockExceptionHandler() {
-//            return (request, response, e) -> {
-//                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-//                Result<String> result = Result.failure("Too many request, please retry later.");
-//                response.getWriter().print(Jackson2Utils.toJson(result));
-//            };
-//        }
-//
-//    }
-//
-//    /**
-//     * 限流、熔断统一处理类
-//     */
-//    @Configuration(proxyBeanMethods = false)
-//    @ConditionalOnClass(ServerResponse.class)
-//    public static class WebfluxHandler {
-//        @Bean
-//        public BlockRequestHandler webfluxBlockExceptionHandler() {
-//            return (exchange, t) ->
-//                    ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
-//                            .contentType(MediaType.APPLICATION_JSON)
-//                            .body(BodyInserters.fromValue(Result.failure(t.getMessage())));
-//        }
-//    }
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass(HttpServletRequest.class)
+    public static class WebmvcHandler {
+        @Bean
+        public BlockExceptionHandler webmvcBlockExceptionHandler() {
+            return (request, response, e) -> {
+                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                Result<String> result = Result.failure("Too many request, please retry later.");
+                response.getWriter().print(Jackson2Utils.toJson(result));
+            };
+        }
+
+    }
+
+    /**
+     * 限流、熔断统一处理类
+     */
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass(ServerResponse.class)
+    public static class WebfluxHandler {
+        @Bean
+        public BlockRequestHandler webfluxBlockExceptionHandler() {
+            return (exchange, t) ->
+                    ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(BodyInserters.fromValue(Result.failure(t.getMessage())));
+        }
+    }
 }
